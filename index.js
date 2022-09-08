44722e77572179abf4563504da6063f2b031c4e4
phili <phili@philis-MacBook-Pro.local>
2022-09-08 19:03:41 +1000

    third commit

--- index.js
+++ index.js
@@ -47,11 +47,37 @@ function scrollFunction() {
   }
 }
 
-function burgerMenu() {
-  var btn = document.getElementsByTagName('button')[0];
-  btn.addEventListener('click', onBtnClick)
+// function burgerMenu() {
+//   var btn = document.getElementsByTagName('button')[0];
+//   btn.addEventListener('click', onBtnClick)
+// }
+
+// function onBtnClick(e) {
+//   this.classList.toggle('opened');
+// }
+
+var elements = document.getElementsByClassName("column");
+
+// Declare a "loop" variable
+var i;
+
+// Full-width images
+function one() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "100%";
+  }
 }
 
-function onBtnClick(e) {
-  this.classList.toggle('opened');
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "25%";
+  }
 }
