f418073ce780c80fc589fff1b1a7a369df7a44c7
phili <phili@philis-MacBook-Pro.local>
2022-09-13 18:43:51 +1000

    This is the fourth commit

--- index.js
+++ index.js
@@ -81,3 +81,36 @@ function four() {
     elements[i].style.flex = "25%";
   }
 }
+
+
+setTimeout(function() {
+  $('#myModal').modal();
+}, 5000);
+
+
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks on <span> (x), close the modal
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onload = function(event) {
+    modal.style.display = "block";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
